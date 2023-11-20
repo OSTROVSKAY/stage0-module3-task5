01eba60464cb11f7072895b5e3890dd0fe4f38a0
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-20 18:24:12 +0300

    Finish : Module #3. Task #5. Operators

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+
     public void convert(float minutes) {
+
+        float seconds = minutes * 60;
+
+        System.out.println(seconds);
+
     }
 }
